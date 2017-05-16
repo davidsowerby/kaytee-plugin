6b697e4ed704023acb65598e864ee5adcab3d748
David Sowerby <david.sowerby@virgin.net>
2017-05-16 09:40:07 +0100

    Improved debugging info for ChangeLogTaskDelegate

--- src/main/groovy/uk/q3c/simplycd/lifecycle/GenerateChangeLogTaskDelegate.groovy
+++ src/main/groovy/uk/q3c/simplycd/lifecycle/GenerateChangeLogTaskDelegate.groovy
@@ -1,5 +1,6 @@
 package uk.q3c.simplycd.lifecycle
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.gradle.api.Project
 import org.gradle.api.logging.LogLevel
 import org.gradle.api.logging.Logger
@@ -11,22 +12,19 @@ import uk.q3c.build.changelog.ChangeLog
 class GenerateChangeLogTaskDelegate {
 
     void generate(Project project, ChangeLog changelog) {
-
-        logConfig(project, changelog)
         SimplyCDProjectExtension config = project.extensions.getByName("simplycd") as SimplyCDProjectExtension
+        logConfig(project, config)
         changelog.configuration = config.changeLog
         changelog.generate()
     }
 
-    private void logConfig(Project project, ChangeLog changelog) {
+    private void logConfig(Project project, SimplyCDProjectExtension config) {
         Logger logger = project.getLogger()
         if (logger.isDebugEnabled()) {
-            StringBuilder buf = new StringBuilder("Generating change log with following config:\n")
-            buf.append("destination file: ${changelog.outputFile()}\n")
-            buf.append("wikiLocal is active: ${changelog.gitPlus().wikiLocal.active}\n")
-            buf.append("wikiLocal create is: ${changelog.gitPlus().wikiLocal.create}\n")
-            buf.append("wikiLocal clone is: ${changelog.gitPlus().wikiLocal.cloneFromRemote}\n")
-            logger.log(LogLevel.DEBUG, buf.toString())
+            ObjectMapper objectMapper = new ObjectMapper()
+            StringWriter sw = new StringWriter()
+            objectMapper.writeValue(sw, config)
+            logger.log(LogLevel.DEBUG, sw.toString())
         }
 
     }
