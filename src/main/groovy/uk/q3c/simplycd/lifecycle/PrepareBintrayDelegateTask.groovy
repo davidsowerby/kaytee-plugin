4971544bf4910cfef043467e820aa144bfc2eba4
David Sowerby <david.sowerby@virgin.net>
2017-05-24 09:23:58 +0100

    Added more properties to prepareBintray config

--- src/main/groovy/uk/q3c/simplycd/lifecycle/PrepareBintrayDelegateTask.groovy
+++ src/main/groovy/uk/q3c/simplycd/lifecycle/PrepareBintrayDelegateTask.groovy
@@ -1,7 +1,13 @@
 package uk.q3c.simplycd.lifecycle
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.jfrog.bintray.gradle.BintrayExtension
+import org.apache.commons.lang.StringUtils
 import org.gradle.api.Project
 import uk.q3c.build.gitplus.remote.DefaultGitRemoteUrlMapper
+
+import java.time.LocalDateTime
 
 /**
  * Created by David Sowerby on 20 May 2017
@@ -18,21 +24,41 @@ class PrepareBintrayDelegateTask {
         SimplyCDProjectExtension simplyCdConfig = project.extensions.getByName("simplycd") as SimplyCDProjectExtension
         DefaultGitRemoteUrlMapper mapper = new DefaultGitRemoteUrlMapper()
         mapper.parent = simplyCdConfig.gitRemoteConfiguration
+        BintrayExtension bintray = project.extensions.getByName("bintray") as BintrayExtension
 
-        project.bintray {
-            publications = ['mavenStuff']
-            pkg {
-                websiteUrl = mapper.repoBaselUrl()
-                issueTrackerUrl = mapper.issuesUrl()
-                vcsUrl = mapper.cloneUrl()
-            }
-            key = project.bintrayKey
+        if (bintray.publications.length == null || bintray.publications.length == 0) {
+            bintray.publications = ['mavenStuff']
+        }
+        if (StringUtils.isEmpty(bintray.pkg.name)) {
+            bintray.pkg.name = simplyCdConfig.gitLocalConfiguration.projectName
+        }
+        if (StringUtils.isEmpty(bintray.pkg.websiteUrl)) {
+            bintray.pkg.websiteUrl = mapper.repoBaselUrl()
+        }
+        if (StringUtils.isEmpty(bintray.pkg.issueTrackerUrl)) {
+            bintray.pkg.issueTrackerUrl = mapper.issuesUrl()
+        }
+        if (StringUtils.isEmpty(bintray.pkg.issueTrackerUrl)) {
+            bintray.pkg.vcsUrl = mapper.cloneUrl()
+        }
+        if (StringUtils.isEmpty(bintray.key)) {
+            bintray.key = project.bintrayKey
+        }
+        if (StringUtils.isEmpty(bintray.pkg.version.name)) {
+            bintray.pkg.version.name = project.version.toString()
+        }
+        if (StringUtils.isEmpty(bintray.pkg.version.released)) {
+            bintray.pkg.version.released = LocalDateTime.now().toString()
         }
 
-        logDebug("bintrayKey key length: " + project.bintray.key.length())
-        logDebug("website: " + project.bintray.pkg.websiteUrl)
-        logDebug("issues: " + project.bintray.pkg.issueTrackerUrl)
-        logDebug("git: " + project.bintray.pkg.vcsUrl)
+        if (project.logger.isDebugEnabled()) {
+            ObjectMapper objectMapper = new ObjectMapper()
+            objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true)
+            StringWriter sw = new StringWriter()
+            objectMapper.writeValue(sw, bintray)
+            project.logger.debug("Bintray config is:\n")
+            project.logger.debug(sw.toString())
+        }
     }
 
     void logDebug(String msg) {
